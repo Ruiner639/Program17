232c269e96e84049c76238e724a79b1a6b8d3c9e
Dmitriy <70809857+Ruiner639@users.noreply.github.com>
2021-01-02 20:41:15 +0300

    Добавьте файлы проекта.

---
+++ Program17/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Program17
+{
+    public class Person
+    {
+        public int age;
+        public Person()
+        {
+            SayHi();
+        }
+        public int SetAge(int n)
+        {
+            age = n;
+            return n;
+        }
+        public void ShowAge()
+        {
+            Console.WriteLine("My age is: " + age + " years old");
+        }
+        public void SayHi()
+        {
+            Console.WriteLine("Hello everybody!");
+        }
+    }
+    public class Student : Person
+    {
+        public Student(int i)
+        {
+            SetAge(i);
+            ShowAge();
+        }
+        
+        public void GoToClasses()
+        {
+            Console.WriteLine("I’m going to class");
+        }
+    }
+    public class Teacher : Person
+    {
+        private string subject;
+        public Teacher(int n)
+        {
+            SetAge(n);
+            ShowAge();
+            Explain();
+        }
+        public void Explain()
+        {
+            Console.WriteLine("Explanation begins");
+        }
+    }
+    class StudentAndTeacherTest
+    {
+        static void Main(string[] args)
+        {
+            Person Robert = new Person();
+            Person Den = new Student(21);
+            Person Teacher = new Teacher(30);
+        }
+    }
+}
